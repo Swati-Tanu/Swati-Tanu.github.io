9d97a5d7c133070e441940b29efa7609328b2fdf
Swati Tanu <swatitani01@gmail.com>
2023-04-11 22:08:33 +0530

    update

--- script.js
+++ script.js
@@ -1,36 +1,7 @@
-// const resumeBtn1=document.getElementById("resume-button-1");
-// const resumeBtn2=document.getElementById("resume-button-2");
-
-// resumeBtn1.addEventListener("click",(e)=>{
-//     setTimeout(()=>{
-//         window.open(window.location.href);
-//         window.location.href = "https://drive.google.com/file/d/1AcxnYYI7awYPNpPfYKPZAK_I_cW6gWmD/view?usp=sharing";
-//     },500);
-// })
-
-// resumeBtn2.addEventListener("click",(e)=>{
-//     setTimeout(()=>{
-//         window.open(window.location.href);
-//         window.location.href = "https://drive.google.com/file/d/1AcxnYYI7awYPNpPfYKPZAK_I_cW6gWmD/view?usp=sharing";
-//     },500);
-// })
-
-function resume(){
-    window.open("./Swati Tanu.pdf","_blank")
-  }
-
-// const resumebtns=document.querySelectorAll(".click");
-// for(resumebtn of resumebtns){
-//     resumebtn.addEventListener("click",(e)=>{
-//         setTimeout(()=>{
-//             window.open("https://drive.google.com/file/d/1AcxnYYI7awYPNpPfYKPZAK_I_cW6gWmD/view?usp=sharing",'_blank');
-//         },500);
-//     })
-// }
-
 const hamburger=document.getElementById("hamb");
 const navbar_menu=document.getElementById("navbar-menu");
 
+//Function for Hamburger Navbar
 hamburger.addEventListener("click", (e)=>{
     if(navbar_menu.style.display=="none"){
         navbar_menu.style.display="block";
@@ -39,6 +10,12 @@ hamburger.addEventListener("click", (e)=>{
     }
 })
 
+//Function for Resume to open on the next tab
+function resume(){
+    window.open("./Swati Tanu.pdf","_blank")
+  }
+
+// Third-party library to disply GitHub Calender
 GitHubCalendar(".calendar", "Swati-Tanu", {
     responsive: true,
     global_stats: true,
